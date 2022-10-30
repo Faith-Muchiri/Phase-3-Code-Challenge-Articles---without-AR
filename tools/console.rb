1fb85448e19095d8ec5a4150689837eb1d8e5bbb
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-10-30 20:20:27 +0300

    add test file

--- tools/console.rb
+++ tools/console.rb
@@ -7,15 +7,18 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+DailyNation = Magazine.new("DailyNation", "News")
+Nairobians = Magazine.new("Nairobians","Trends")
+article1 = Article.new("WaliBora", "Nation", "leadership")
+article2 = Article.new("Margret Agola", "Nairobians", "culture")
+author1 = Author.new("Ken WaliBora")
+author2 = Author.new("Margret Agola")
 
 
 
 
 
-
-
-
-### DO NOT REMOVE THIS
+### DO NOT REMOVE THISora
 binding.pry
 
 0
