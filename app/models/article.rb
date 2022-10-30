5d7cd6ae8a3428fe30da8ec06d040240071da002
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-10-30 20:19:53 +0300

    modify article file

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,11 @@
 class Article
+    attr_accessor :author, :magazine, :title 
+    
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+    end
 
 
 end
